c940b41d7367e87f267192fcb0ea5da6cdae011a
Michael Deng <michaeldeng18@gmail.com>
2015-03-01 16:16:31 -0800

    Add first tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "UC Berkeley Alpha Kappa Psi"
+  def full_title(page_title = '')
+    base_title = "VentureInspire"
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{page_title} | #{base_title}"
     end
   end
 end
