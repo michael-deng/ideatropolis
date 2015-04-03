1567ff16ae46848ce0296191f336c0fe48dd0b9e
Michael Deng <michaeldeng18@gmail.com>
2015-04-02 23:29:02 -0700

    Add index page for ideas

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,11 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+
+users = User.order(:created_at).take(6)
+
+50.times do
+	title = Faker::Lorem.words(3)
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.ideas.create!(title: title, content: content) }
+end
