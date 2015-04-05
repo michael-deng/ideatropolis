975bdac891f5ad44ac7dd2338ddb240baf9f3653
Michael Deng <michaeldeng18@gmail.com>
2015-04-04 18:07:45 -0700

    Add new ideas show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ end
 users = User.order(:created_at).take(6)
 
 50.times do
-	title = Faker::Lorem.words(3)
+	title = Faker::Lorem.word
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.ideas.create!(title: title, content: content) }
 end
