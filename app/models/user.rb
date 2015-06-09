94d74ddae04c19d7ccbe6cd0bcae9e133896edc5
Michael Deng <michaeldeng18@gmail.com>
2015-06-08 23:49:16 -0700

    Add from_omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,14 +18,16 @@ class User < ActiveRecord::Base
   									format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
-end
 
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
   end
 end
+
+
 
