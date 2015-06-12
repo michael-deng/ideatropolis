51dcf94aca05ffabcb066ee25797d5bf92d2610f
michael-deng <michaeldeng18@gmail.com>
2015-06-12 13:29:28 -0700

    Finish integration of avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   has_attached_file :avatar, 
-                        :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                        :default_url => "/images/:style/missing.png",
+                        :styles => { :medium => "250x250>", :icon => "25X25>" }, 
+                        :default_url => "default_:style_avatar.jpg",
                         :storage => :s3,
                         :s3_credentials => {
                           :bucket => ENV['S3_BUCKET_NAME'],
