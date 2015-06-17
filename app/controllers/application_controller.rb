0a6c7910786a937916331b78f7a06cf06b3e6927
michael-deng <michaeldeng18@gmail.com>
2015-06-17 10:40:57 -0700

    Redirect sign up to choose interests page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if resource.sign_in_count == 1
+       edit_category_registration_path(resource)
+    else
+       super
+    end
+	end
+
   protected
 
 	  def configure_permitted_parameters
