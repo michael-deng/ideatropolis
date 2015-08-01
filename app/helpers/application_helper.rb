1817b5f4562739f76ead49742abb3a8b4fe9a14c
michael-deng <michaeldeng18@gmail.com>
2015-08-01 09:56:17 -0700

    Make searchbar smaller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "UnicornBase"
+    base_title = "Unicornbase"
     if page_title.empty?
       base_title
     else
