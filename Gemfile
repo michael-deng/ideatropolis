7f5bf5e1c216e074302f0961e2de8066ddc2fe7f
Michael Deng <michaeldeng18@gmail.com>
2015-11-18 13:17:27 -0800

    Finish idea voting functionality

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem "paperclip", "~> 4.2"
 gem 'aws-sdk', '< 2.0'
 gem 'kaminari'
 gem 'acts_as_votable', '~> 0.10.0'
+gem 'jquery-turbolinks'
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,14 +11,36 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require best_in_place
+
 $(document).ready(function() {
-  /* Activating Best In Place */
+
+	/* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
+
+  function handler1() {
+  	$(this).css('color', '#00AF33');
+		var el = $(this).find('.vote-count');
+	  var num = parseInt(el.text());
+	  el.text(num+1);
+	  $(this).one("click", handler2);
+	}
+
+	function handler2() {
+		$(this).css('color', 'black');
+	  var el = $(this).find('.vote-count');
+	  var num = parseInt(el.text());
+	  el.text(num-1);
+	  $(this).one("click", handler1);
+	}
+
+	$(".voted").one("click", handler2);
+	$(".novote").one("click", handler1);
+
 });
-//= require turbolinks
 
 /* Activating Google analytics */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -29,7 +51,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-55613013-3', 'auto');
 ga('send', 'pageview');
 
-//= require_tree .
+//= require turbolinks
 
 
 
